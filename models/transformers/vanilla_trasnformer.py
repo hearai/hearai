1a4d2ae3a5b6dc6f0bf25073e9afd008820b5d12
alicjak1519 <akrzeminska1519@gmail.com>
2021-12-21 02:27:48 +0100

    Add initial implementation of ModelLoader class for feature extractors and transformers uploading

--- models/transformers/vanilla_trasnformer.py
+++ models/transformers/vanilla_trasnformer.py
@@ -1,20 +1,22 @@
 import timm
 import torch.nn as nn
 
-class Model(nn.Module):
+
+class VanillaTransformer(nn.Module):
     """Basic transformer model"""
 
-    def __init__(self,input_size=128,output_size=1000):
+    def __init__(self, input_size=128, output_size=1000):
         """
         Dummy example of __init__ function of basic transformer model. Does nothing as transformer is not even implemented.
         
         Args:
             input_size (int, optional): Input size fot the transformer model. Should be equal to the output size of the feature_extrator. Defaults to 128.
             output_size (int, optional): Output size. Should be equal to the selected representation size. Defaults to 1000.
-        """        super().__init__()
-        self.transformer = transfomer(input_size, output_size) # TO-DO
+        """
+        super().__init__()
+        self.transformer = transfomer(input_size, output_size)  # TO-DO
         # this is a dummy example but in practice this will be longer 
-      
+
     def forward(self, input, **kwargs):
         # this is a dummy example but in practice this will be longer 
-        return  self.transformer(input)
+        return self.transformer(input)
