a938e0af9925741c9ef39e694322acf83216fcab
Agnieszka Mikołajczyk <midnight.inn@gmail.com>
2021-12-21 11:11:46 +0100

    Update dataset.py

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -6,23 +6,23 @@ class JsonlDataset(Dataset):
         Args:
             data_path ([type]): Path to Jsonl file
         """      
-        # loaded_json = []
-        # with open(data_path) as f:
-        #     for line in f:
-        #         loaded_json.append(json.loads(line))
-        # data_pairs = []
-        # for row in loaded_json[:max_len]:
-        #     hand_coordinate = row["hand_coordinate"]
-        #     face_mesh = row["face_mesh"]
-        #     data_pairs.append([hand_coordinate, face_mesh])
+        loaded_json = []
+        with open(data_path) as f:
+            for line in f:
+                loaded_json.append(json.loads(line))
+        data_pairs = []
+        for row in loaded_json[:max_len]:
+            hand_coordinate = row["hand_coordinate"]
+            face_mesh = row["face_mesh"]
+            data_pairs.append([hand_coordinate, face_mesh])
 
-        # self.samples = data_pairs
+        self.samples = data_pairs
 
     def __len__(self):
         return 10
 
     def __getitem__(self, idx):
-        # sample = {}
-        # sample["hand_coordinate"] = self.samples[idx][0]
-        # sample["face_mesh"] = self.samples[idx][1]
+        sample = {}
+        sample["hand_coordinate"] = self.samples[idx][0]
+        sample["face_mesh"] = self.samples[idx][1]
         return None
