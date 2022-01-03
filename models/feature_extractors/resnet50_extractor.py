3d49b634778f787161ae8430a600b6fa1986653c
mariaogryczak <37352032+mariaogryczak@users.noreply.github.com>
2022-01-03 12:28:05 +0100

    Update resnet50_extractor.py

--- models/feature_extractors/resnet50_extractor.py
+++ models/feature_extractors/resnet50_extractor.py
@@ -6,5 +6,6 @@ class Resnet50Extractor(nn.Module):
     def __init__(self, representation_size=256, model_path='resnet50'): 
         super().__init__() 
         self.model = timm.create_model(model_path, pretrained=True, num_classes=representation_size)
+        
     def forward(self, x, **kwargs):
         return self.model(x)
