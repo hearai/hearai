04161ae797764f98320917f631722764643e1577
Wiktor_Filipiuk <wiktor.filipiuk@gmail.com>
2022-01-09 17:00:31 +0100

    Added PretrainedTransformer class.

--- models/transformers/vanilla_transformer.py
+++ models/transformers/vanilla_transformer.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.optim
 
 
-class TransformerModel(nn.Module):
+class VanillaTransformer(nn.Module):
     """Basic transformer model"""
 
     def __init__(self,
@@ -16,7 +16,7 @@ class TransformerModel(nn.Module):
             input_size (int, optional): Input size fot the transformer model. Should be equal to the output size of the feature_extrator. Defaults to 128.
             output_size (int, optional): Output size. Should be equal to the selected representation size. Defaults to 1000.
         """
-        super().__init__()
+        super(VanillaTransformer, self).__init__()
         self.__fully_connected = nn.Linear(input_size, output_size)
         self.__output_layer = nn.Softmax(dim=1)
         # this is a dummy example but in practice this will be longer 
