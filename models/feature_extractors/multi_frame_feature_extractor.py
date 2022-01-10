4814c641ac2ce27a17ad72bbdb5e06ce151f1882
Sylwia Majchrowska <majsylw@onet.eu>
2022-01-10 23:26:05 +0100

    adapt datasetlader

--- models/feature_extractors/multi_frame_feature_extractor.py
+++ models/feature_extractors/multi_frame_feature_extractor.py
@@ -4,10 +4,8 @@ import torch.nn as nn
 class MultiFrameFeatureExtractor(nn.Module):
     """
     Class to extract features from every frame of the video.
-
-    Input to the model should have shape NxCxHxW (frames x channels x height x width).
-
-    Output from the model is a Tensor of shape NxM (count_of_frames x representation_size).
+    Input to the model should have shape BxNxCxHxW (batch x frames x channels x height x width).
+    Output from the model is a Tensor of shape NxM (batch x representation_size).
 
     Args:
         feature_extractor_name (str): the feature extractor.
@@ -15,22 +13,15 @@ class MultiFrameFeatureExtractor(nn.Module):
 
     def __init__(self,
                  feature_extractor):
-
         super().__init__()
-
-        
         self.feature_extractor = feature_extractor
-
 
     def forward(self, x):
         frame_features = []
 
         for frame in x:
-            frame = frame.unsqueeze(0)
             output = self.feature_extractor(frame).squeeze(0)
             frame_features.append(output)
 
         frame_features = torch.stack(frame_features, dim=0)
-            
         return frame_features
-
--- train.py
+++ train.py
@@ -1,32 +1,39 @@
-import os
 import argparse
 import torch
 import yaml
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader, Dataset, random_split
 from models.model import GlossTranslationModel
-from torchvision.datasets.fakedata import FakeData
 import torchvision.transforms as T
-
 import os
+
+from datasets.dataset import ImglistToTensor, VideoFrameDataset
 
 def get_args_parser():
     parser = argparse.ArgumentParser()
     # Data parameters and paths
     parser.add_argument('--data',
-                        help='path to data')
+                        help='path to data',
+                        default='/dih4/dih4_2/hearai/data/frames/pjm')
+    parser.add_argument('--classes', type=int, default=2400,
+                        help='number of classes')
+    parser.add_argument('--ratio', type=float, default=0.9,
+                        help='train/test ratio (default: 0.9)')
     # Training parameters
     parser.add_argument('--lr', type=float, default=3e-5,
                         help='learning rate (default: 3e-5)')
     parser.add_argument('-b', '--batch-size', type=int, default=8,
-                        help='input batch size for training (default: 64)')
+                        help='input batch size for training (default: 8)')
     parser.add_argument('--epochs', type=int, default=10, metavar='EPOCHS',
-                        help='number of epochs to train (default: 1000)')
+                        help='number of epochs to train (default: 10)')
     parser.add_argument('--workers', type=int, default=0,
                         help='number of parallel workers (default: 0)')
     # Other
     parser.add_argument('--gpu', type=int, default=0,
                         help='number of GPU to use')
+    parser.add_argument('--save',
+                        help='path to save model',
+                        default='./best.pth')
     parser.add_argument('--seed', type=int, default=2021,
                         help='random seed (default: 2021)')
     return parser
@@ -45,12 +52,28 @@ def main(args):
     test_transforms = T.Compose([
     T.PILToTensor(),
     T.ConvertImageDtype(torch.float)])
-    
-    # load fake data
-    dataset = FakeData(image_size=(3,256,256), transform=test_transforms)
+
+    # load data
+    videos_root = args.data
+    annotation_file = os.path.join(videos_root, 'annotations.txt')
+    preprocess = T.Compose([
+        ImglistToTensor(),  # list of PIL images to (FRAMES x CHANNELS x HEIGHT x WIDTH) tensor
+        T.Resize(256),  # image batch, resize smaller edge to 256
+        T.CenterCrop(256),  # image batch, center crop to square 256x256
+        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    dataset = VideoFrameDataset(
+        root_path=videos_root,
+        annotationfile_path=annotation_file,
+        num_segments=1,
+        frames_per_segment=1,
+        transform=preprocess,
+        test_mode=False
+    )
 
     # split into train/val
-    train_val_ratio = 0.9
+    train_val_ratio = args.ratio
     train_len = round(len(dataset) * train_val_ratio)
     val_len = len(dataset) - train_len
     train, val = random_split(dataset, [train_len, val_len])
@@ -63,7 +86,10 @@ def main(args):
                                 num_workers=args.workers, drop_last=False)
 
     # prepare model
-    model = GlossTranslationModel(lr=args.lr, feature_extractor_name="resnet50_extractor")
+    model = GlossTranslationModel(lr=args.lr,
+                                  num_classes=args.classes,
+                                  feature_extractor_name="cnn_extractor",
+                                  model_save_dir=args.save)
 
     # create NeptuneLogger
     # TO - DO
