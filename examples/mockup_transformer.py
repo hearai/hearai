0cabd160a02b434151d792c17cec7d0e5fa270ee
Wiktor_Filipiuk <wiktor.filipiuk@gmail.com>
2022-01-15 15:24:37 +0100

    Added PretrainedTransformer.

--- examples/mockup_transformer.py
+++ examples/mockup_transformer.py
@@ -3,17 +3,14 @@ import torch.nn as nn
 
 from torch.optim import Adam
 
-from hearai.mockups.transformer_mockups import (
-    generate_mockup_input,
-    generate_mockup_output,
-)
-from hearai.models.transformers.vanilla_trasnformer import TransformerModel
+from mockups.transformer_mockups import generate_mockup_input, generate_mockup_output
+from models.transformers.pretrained_transformer import PretrainedTransformer
 
 
-batch_size = 2
-input_shape = (128,)
-value_range = (0.0, 1.0)
-num_classes = 10
+batch_size = 4
+input_shape = (7, 512,)
+value_range = (0., 1.)
+num_classes = 2
 
 feature_extractor_output = generate_mockup_input(
     batch_size=batch_size, input_shape=input_shape, value_range=value_range
@@ -21,7 +18,11 @@ feature_extractor_output = generate_mockup_input(
 labels = generate_mockup_output(batch_size=batch_size, num_classes=num_classes)
 
 # simulate model training
-model = TransformerModel(input_size=128, output_size=10)
+checkpoint = 'facebook/hubert-large-ls960-ft'
+encoder_list = [64, 10, 2]
+model = PretrainedTransformer(checkpoint=checkpoint,
+                              encoder_list=encoder_list,
+                              add_softmax=True)
 loss_fn = nn.functional.cross_entropy
 optimizer = Adam(model.parameters(), lr=1e-4)
 epochs = 1000
@@ -34,8 +35,8 @@ for epoch in range(epochs):
     loss.backward()
     optimizer.step()
     optimizer.zero_grad()
-    if (epoch % 10) == 0:
-        print(f"Epoch {epoch}/{epochs}, loss: {loss}")
+    if (epoch % 1) == 0:
+        print(f'Epoch {epoch}/{epochs}, loss: {loss}')
 
 print(f"Predicted labels: {np.argmax(model(X).detach().numpy(), axis=1)}")
 print(f"True labels: {np.argmax(y_true, axis=1)}")
