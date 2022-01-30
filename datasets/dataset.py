fix gradient leaf error, gpu training error, dataset with header loading error

datasets/dataset.py
@@ -141,7 +141,12 @@ class VideoFrameDataset(torch.utils.data.Dataset):
         ]
 
     def _sanity_check_samples(self):
-        for record in self.video_list:
+        for id, record in enumerate(self.video_list):
+            if any(isinstance(x, str) for x in record._data):
+                # Found datafile header. Removing it.
+                del self.video_list[id]
+                continue
+                
             if record.num_frames <= 0 or record.start_frame == record.end_frame:
                 print(
                     f"\nDataset Warning: video {record.path} seems to have zero RGB frames on disk!\n"

models/feature_extractors/multi_frame_feature_extractor.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 
+from torch.autograd import Variable
+
 
 class MultiFrameFeatureExtractor(nn.Module):
     """
@@ -15,13 +17,13 @@ class MultiFrameFeatureExtractor(nn.Module):
     def __init__(self, feature_extractor):
         super().__init__()
         self.feature_extractor = feature_extractor
-
+        
     def forward(self, x):
         frame_features = []
 
         for frame in x:
             output = self.feature_extractor(frame).squeeze(0)
-            frame_features.append(output)
+            frame_features.append(Variable(output))
 
         frame_features = torch.stack(frame_features, dim=0)
         return frame_features