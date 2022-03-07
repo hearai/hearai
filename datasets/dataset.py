75d96526978a72b7ccd4a9b72cc6380ddcceee5c
Sylwia Majchrowska <majsylw@onet.eu>
2022-03-07 22:09:56 +0100

    modify path to read basic video properties

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -26,7 +26,8 @@ ALL_HAMNOSYS_HEADS = {"symmetry_operator": 3,
             "hand_position_finger_direction": 7,
             "hand_position_palm_orientation": 8,
             "hand_location_x": 9,
-            "hand_location_y": 10}
+            "hand_location_y": 10,
+            "distance": 11}
 
 class VideoRecord(object):
     """
@@ -45,13 +46,13 @@ class VideoRecord(object):
              5) any following elements are labels in the case of multi label provided.
     """
 
-    def __init__(self, row, root_datapath, classification_heads, landmarks_path=None):
+    def __init__(self, row, root_datapath, classification_heads):
         self._data = row
         self._path = os.path.join(root_datapath, row[0])
         self.classification_heads = classification_heads
         try:
             with open(
-                os.path.join(landmarks_path, row[0] + "_properties.json"), "r"
+                os.path.join(self._path, row[0] + "_properties.json"), "r"
             ) as f:
                 data = json.load(f)
             self.fps = int(data["FPS"])
@@ -107,8 +108,9 @@ class VideoFrameDataset(torch.utils.data.Dataset):
         root_path: The root path in which video folders lie.
         annotationfile_path: The .txt annotation file containing
                              one row per video sample as described above.
-        classification_mode: mode for classification, choose from
-                             classification_mode.py
+        is_pretraining: flag to launch pretrianing block
+        classification_heads: dict with names of classification heads
+                              and number of their classes
         num_segments: The number of segments the video should
                       be divided into to sample frames from.
         frames_per_segment: The number of frames that should
@@ -127,9 +129,9 @@ class VideoFrameDataset(torch.utils.data.Dataset):
         self,
         root_path: str,
         annotationfile_path: str,
-        classification_mode: str,
         is_pretraining: bool,
-        classification_heads: {},
+        classification_mode: str,
+        classification_heads={},
         num_segments: int = -1,
         frames_per_segment: int = 1,
         time: Union[float, None] = None,
@@ -167,7 +169,7 @@ class VideoFrameDataset(torch.utils.data.Dataset):
     def _parse_annotationfile(self):
         self.video_list = [
             VideoRecord(x.strip().split(), self.root_path,
-                        self.classification_heads, self.landmarks_path)
+                        self.classification_heads)
             for x in open(self.annotationfile_path)
         ]
 
