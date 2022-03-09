3877f4599ad737eca1fd420a76e4b9f5badf1a7a
greg <greg@polquant.pl>
2022-03-10 00:49:41 +0100

    training conf change

--- train.py
+++ train.py
@@ -213,7 +213,7 @@ def main(args):
                            feedforward_size=model_config["transformer"]["feedforward_size"],
                            num_encoder_layers=model_config["transformer"]["num_encoder_layers"],
                            transformer_output_size=model_config["transformer"]["output_size"],
-                           warmup_steps=5000,
+                           warmup_steps=50000,
                            warmup_start_lr=5.0e-6,
                            multiply_lr_step=1,
                            freeze_scheduler=model_config["freeze_scheduler"]
