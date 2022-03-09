ca75efb65238e3e27cd3824e8656c1dd51a34e9d
Sylwia Majchrowska <majsylw@onet.eu>
2022-03-09 15:52:54 +0100

    code refactore

--- utils/make_statistics.py
+++ utils/make_statistics.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 from argparse import ArgumentParser
 
+
 def main(file_path: str, output_directory: str, separator: str):
     df = pd.read_csv(file_path, sep=separator)
 
@@ -13,12 +14,12 @@ def main(file_path: str, output_directory: str, separator: str):
     df = df.drop("start", axis="columns")
     df = df.drop("end", axis="columns")
     df = df.dropna(axis="columns")
-    
+
     for column_name in df.columns:
         file_name = column_name.replace(" ", "_").replace("/", "_")
-        file_name = file_name+".png"
+        file_name = file_name + ".png"
         file_path = os.path.join(output_directory, file_name)
-        
+
         column_stats = df.loc[:, column_name].value_counts().sort_index()
 
         # Set figure size to plot a more data
@@ -37,12 +38,22 @@ def main(file_path: str, output_directory: str, separator: str):
 
 
 if __name__ == "__main__":
-    parser = ArgumentParser(description="AA")
+    parser = ArgumentParser(description="Generate stats for HamNoSys files")
 
-    parser.add_argument("--file_path", type=str, help="Path to file, which contains HamNoSys annotations")
-    parser.add_argument("--output_directory", type=str, help="Path to the output directory")
+    parser.add_argument(
+        "--file_path",
+        type=str,
+        help="Path to file, which contains HamNoSys annotations",
+    )
+    parser.add_argument(
+        "--output_directory", type=str, help="Path to the output directory"
+    )
 
-    parser.add_argument("--separator", default=" ", help="Sign, which is used as a separator in the annotation file")
+    parser.add_argument(
+        "--separator",
+        default=" ",
+        help="Sign, which is used as a separator in the annotation file",
+    )
     args = parser.parse_args()
-    
+
     main(args.file_path, args.output_directory, args.separator)
