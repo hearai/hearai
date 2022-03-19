cfb3b98344aeb2ce51e3c236302fc5fe40f7692e
Agnieszka Mikołajczyk <midnight.inn@gmail.com>
2022-03-19 20:32:26 +0100

    update lstm  docstring

--- models/transformers/lstm.py
+++ models/transformers/lstm.py
@@ -2,18 +2,24 @@ import torch
 class LSTM(torch.nn.Module):
     """Basic transformer model"""
 
-    def __init__(self, input_size: int,
+    def __init__(self, 
+                 input_size: int,
                  output_size: int,
-                 feedforward_size,
-                 num_encoder_layers,
-                 num_frames,
-                 dropout_rate):
+                 num_frames: int,
+                 feedforward_size = 512,
+                 num_encoder_layers = 2,
+                 dropout_rate = 0.2):
         """
-        Dummy example of __init__ function of basic transformer model. Does nothing as transformer is not even implemented.
+        Lightweight function of basic LSTM model
         
         Args:
-            input_size (int, optional): Input size fot the transformer model. Should be equal to the output size of the feature_extrator. Defaults to 128.
-            output_size (int, optional): Output size. Should be equal to the selected representation size. Defaults to 1000.
+            input_size (int): Input size fot the LSTM model. Should be equal to the output size of the feature_extrator.
+            output_size (int): Output size. Should be equal to the selected representation size. 
+            num_frames (int): Number of input frames.
+            feedforward_size (int): Hidden size of an LSTM model. Defaults to 512.
+            num_encoder_layers (int): Number of LSTM layers. Defaults to 2.
+            dropout_rate (float): Dropout stats. Should be between 0 and 1. Deafults to 0.2.
+            
         """
         super().__init__()
         self.LSTM = torch.nn.LSTM(input_size=input_size,
