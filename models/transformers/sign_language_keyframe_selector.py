b4cab98dde861270e8609b67502effcda9ac3437
greg <greg@polquant.pl>
2022-04-20 01:02:33 +0200

    refactoring end extending

--- models/transformers/sign_language_keyframe_selector.py
+++ models/transformers/sign_language_keyframe_selector.py
@@ -40,54 +40,105 @@ class SignLanguageKeyframeSelector(nn.Module):
         super(SignLanguageKeyframeSelector, self).__init__()
         self._input_size = feature_extractor_parameters["representation_size"]
         self._attention_heads = transformer_parameters["num_attention_heads"]
+        self._num_encoder_layers = transformer_parameters["num_encoder_layers"]
+        self._dropout_rate = transformer_parameters["dropout_rate"]
 
+        self._velocity_modules = nn.ModuleList()
+        self._mixing_layers = nn.ModuleList()
+        self._acceleration_modules = nn.ModuleList()
 
-        self._cnn_layers = nn.Conv2d(in_channels=self._attention_heads,
-                                     out_channels=self._attention_heads,
-                                     groups=self._attention_heads,
-                                     kernel_size=(5, 1),
-                                     padding='same',
-                                     padding_mode='replicate')
+        for i in range(self._num_encoder_layers):
+            velocity_module = nn.ModuleList()
+            velocity_module.append(
+                nn.Conv2d(in_channels=self._attention_heads,
+                          out_channels=self._attention_heads,
+                          groups=self._attention_heads,
+                          kernel_size=(3, 8),
+                          stride=(1, 8),
+                          padding=(1, 0),
+                          padding_mode='replicate')
+            )
+            velocity_module.append(
+                nn.ELU(0.1)
+            )
+            self._velocity_modules.append(velocity_module)
 
-        self._importance_calculator = nn.Conv2d(in_channels=self._attention_heads,
-                                     out_channels=self._attention_heads,
-                                     groups=self._attention_heads,
-                                     kernel_size=(1, 2 * self._input_size),
-                                     padding='valid',
-                                     padding_mode='replicate')
-        self._importance_dropout = nn.Dropout(transformer_parameters["dropout_rate"])
+            if i > 0:
+                acceleration_module = nn.ModuleList()
+                acceleration_module.append(
+                    nn.Conv2d(in_channels=self._attention_heads,
+                              out_channels=self._attention_heads,
+                              groups=self._attention_heads,
+                              kernel_size=(3, 1),
+                              padding='same',
+                              padding_mode='replicate')
+                )
+                acceleration_module.append(
+                    nn.ELU(0.1)
+                )
+                self._acceleration_modules.append(acceleration_module)
+
+            mixing_module = nn.ModuleList()
+            mixing_module.append(
+                nn.Dropout(self._dropout_rate)
+            )
+            mixing_module.append(
+                nn.Conv2d(in_channels=self._attention_heads,
+                          out_channels=self._attention_heads,
+                          groups=self._attention_heads,
+                          kernel_size=(1, int((i + 1) * self._input_size / 8)),
+                          padding='same',
+                          padding_mode='replicate')
+            )
+            mixing_module.append(
+                nn.ELU(0.1)
+            )
+            self._mixing_layers.append(mixing_module)
+
+        self._importance_dropout = nn.Dropout(self._dropout_rate)
+        self._importance_calculator = nn.Conv2d(
+            in_channels=self._attention_heads,
+            out_channels=self._attention_heads,
+            groups=self._attention_heads,
+            kernel_size=(1, int(self._input_size * (0 + self._num_encoder_layers / 8)))
+        )
         self._importance_activation = nn.Softmax(dim=-2)
 
-        self._sequential = SimpleSequentialModel(layers=2,
-                                                 representation_size=transformer_parameters["output_size"],
-                                                 dropout_rate=transformer_parameters["dropout_rate"])
+        self._final_sequential = SimpleSequentialModel(layers=2,
+                                                       representation_size=transformer_parameters["output_size"],
+                                                       dropout_rate=transformer_parameters["dropout_rate"])
 
     def forward(self, input: torch.Tensor):
 
         shape = input.shape
-        input = torch.reshape(input, (shape[0], 1, shape[1], shape[2]))
+        input0 = torch.reshape(input, (shape[0], 1, shape[1], shape[2]))
 
         if self._attention_heads > 1:
-            input = torch.concat([input for _ in range(self._attention_heads)], 1)
+            input0 = torch.concat([input0 for _ in range(self._attention_heads)], 1)
 
-        x = self._cnn_layers(input)
+        for i in range(self._num_encoder_layers):
+            v = input0
+            for v_layer in self._velocity_modules[i]:
+                v = v_layer(v)
 
-        x = torch.concat(
-            [
-                input,
-                torch.multiply(x, x)
-            ],
-            dim=-1
-        )
-        x = self._importance_dropout(x)
-        x = self._importance_calculator(x)
-        x = self._importance_activation(x)
+            if i == 0:
+                a = v
+            else:
+                for a_layer in self._acceleration_modules[i - 1]:
+                    a = a_layer(a)
+                a = torch.concat([v, a], dim=-1)
 
-        x = torch.multiply(x, input)
+            for m_layer in self._mixing_layers[i]:
+                a = m_layer(a)
+
+        w = self._importance_dropout(a)
+        w = self._importance_calculator(w)
+        w = self._importance_activation(w)
+
+        x = torch.multiply(w, input0)
         x = torch.sum(x, dim=-2)
-
         x = torch.flatten(x, start_dim=1)
 
-        x = self._sequential(x)
+        x = self._final_sequential(x)
 
         return x
